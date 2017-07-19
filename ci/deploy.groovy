132a6ee2a900c88b4a685214c52936080f2b5391
John Crocker <jcrocker@zenoss.com>
2017-07-19 10:55:35 -0500

    Add ci-related files

---
+++ ci/deploy.groovy
@@ -0,0 +1,21 @@
+MAKE='make -f Makefile -f ci/ci.mk'
+node('docker') {
+	git url: 'https://github.com/zenoss/zing-hdfs.git'
+
+    configFileProvider([
+        configFile(fileId: 'global', variable: 'GLOBAL'),
+    ]){
+        global = load env.GLOBAL
+    }
+    service = load 'ci/service.groovy'
+    sh('git rev-parse --short=8 HEAD > ci/.image_tag')
+
+    stage('Build image') {
+		sh("${MAKE} build")
+    }
+
+    stage('Publish image as "latest"') {
+		def imageName = "${global.PUBLISHER_DEVELOP}/${service.DOCKER_IMAGE}:latest"
+		sh("${MAKE} push REMOTE_IMAGE=${imageName}")
+    }
+}
